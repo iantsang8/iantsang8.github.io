a9ed81355162972306d9904c07ca8bbd4e7b0633
iantsang8 <164898576+iantsang8@users.noreply.github.com>
2024-07-29 16:24:50 +0800

    Update homejs.js

--- homejs.js
+++ homejs.js
@@ -75,7 +75,7 @@ function eraseText() {
 }
 
 // typing animation
-window.onload = startTyping;
+window.addEventListener('load', startTyping);
 
 window.addEventListener('resize', checkWidth);
 
@@ -91,7 +91,7 @@ window.addEventListener('scroll', function() {
 });
 
 // for image tilt
-const image = document.querySelector(".profile img");
+const image = document.querySelector("#profile img");
 let imageRect = image.getBoundingClientRect();
 
 image.addEventListener("mouseenter", () => {
@@ -124,3 +124,36 @@ image.addEventListener("mouseleave", () => {
     image.style.transform = "translate(-50%, -50%) perspective(1000px) rotateX(0deg) rotateY(0deg) scale3d(1, 1, 1)";
 });
 
+
+function handleIntersection(entries, observer) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('is-visible');
+            entry.target.classList.remove('is-hidden');
+        } else {
+            entry.target.classList.remove('is-visible');
+            entry.target.classList.add('is-hidden');
+        }
+    });
+}
+
+const observer = new IntersectionObserver(handleIntersection, {
+    root: null,
+    rootMargin: '0px', // This will trigger the effect slightly before the element is fully visible/invisible
+    threshold: 0
+});
+
+const observerAbout = new IntersectionObserver(handleIntersection, {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.3
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const animatedElements = document.querySelectorAll('.animate-on-scroll');
+    animatedElements.forEach(el => observer.observe(el));
+    const animatedProfile = document.querySelectorAll('.animate-profile');
+    animatedProfile.forEach(el => observer.observe(el));
+    const animatedAbout = document.querySelectorAll('.animate-about');
+    animatedAbout.forEach(el => observerAbout.observe(el));
+});
